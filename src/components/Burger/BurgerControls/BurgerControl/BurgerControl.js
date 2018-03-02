b7de78073fc0adfaef835ab5d20283379817f823
LilianKalkman <liliankalkman@hotmail.com>
2018-03-02 17:22:14 +0100

    styling

--- src/components/Burger/BurgerControls/BurgerControl/BurgerControl.js
+++ src/components/Burger/BurgerControls/BurgerControl/BurgerControl.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import classes from './BurgerControl.css';
 
 const burgerControl = (props) => {
   return(
-    <div>control</div>
+    <div className={classes.BurgerControl}>
+      <div className={classes.Label}>props.label</div>
+      <button className={classes.Less}>Less</button>
+      <button className={classes.More}>More</button>
+    </div>
   );
 }
 
