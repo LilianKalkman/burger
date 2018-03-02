a4aa49903ac44a378af5bb4a25c742590e4b3967
LilianKalkman <liliankalkman@hotmail.com>
2018-03-02 14:52:10 +0100

    burger with state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,15 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.css';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+  .map(ingrKey => {
+    return [...Array(props.ingredients[ingrKey])].map((_, i) => {
+      return <BurgerIngredient key={ingrKey + i} type={ingrKey} />;
+    });
+  });
   return(
     <div className={classes.Burger}>
-      <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bread-bottom" />
+      {transformedIngredients}
     </div>
   );
 }
