608027c554f4742ab68fff26f6ad86ecbaaa8c76
LilianKalkman <liliankalkman@hotmail.com>
2018-03-02 16:06:31 +0100

    ingredients dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.css';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
   .map(ingrKey => {
     return [...Array(props.ingredients[ingrKey])].map((_, i) => {
       return <BurgerIngredient key={ingrKey + i} type={ingrKey} />;
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+
+  if(transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients</p>
+  };
+
+  console.log(transformedIngredients);
+
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
