7e765ff58bcd3ae1d3d6557cf421569f04b51ec3
LilianKalkman <liliankalkman@hotmail.com>
2018-03-06 13:09:28 +0100

    lkhdg;ash

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  constructor(props){
+    super(props);
 
-export default layout;
+    this.state = {
+      showSideDrawer: true
+    }
+  }
+
+  removeSideDrawerHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  render(){
+    let sideDrawer = null;
+    if(this.state.showSideDrawer){
+      sideDrawer =  <SideDrawer
+          close={this.removeSideDrawerHandler}
+          open={this.state.showSideDrawer}/>
+    }
+
+    return(
+      <Aux>
+        <Toolbar />
+        {sideDrawer}
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
 
 // hier (in main>props.children) komen je components die je wilt laten zien en wrapped in je layout, dus roep je de children
